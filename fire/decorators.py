9ffff6e74406aed6f1a32593bd657570d3a9d9cd
Restyled.io <commits@restyled.io>
2024-05-05 11:11:21 +0000

    Restyled by reorder-python-imports

--- fire/decorators.py
+++ fire/decorators.py
@@ -11,14 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """These decorators provide function metadata to Python Fire.
 
 SetParseFn and SetParseFns allow you to set the functions Fire uses for parsing
 command line arguments to client code.
 """
-
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import inspect
 
